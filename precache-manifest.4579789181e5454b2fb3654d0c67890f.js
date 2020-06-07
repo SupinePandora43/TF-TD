18a340bd1fe4dc8f50d7a1825881a36172823d77
SupinePandora43 <SupinePandora43@users.noreply.github.com>
2020-06-07 05:15:52 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.4579789181e5454b2fb3654d0c67890f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c56ead3a91eb6cc00783c713a7cbdf21",
+    "url": "/TF-TD/index.html"
+  },
+  {
+    "revision": "d65443cc1fb5f43b5427",
+    "url": "/TF-TD/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "219058648073693a2dcd",
+    "url": "/TF-TD/static/js/2.3ae456c8.chunk.js"
+  },
+  {
+    "revision": "d65443cc1fb5f43b5427",
+    "url": "/TF-TD/static/js/main.119a595c.chunk.js"
+  },
+  {
+    "revision": "afcb8e61d761e3d1d954",
+    "url": "/TF-TD/static/js/runtime-main.35899227.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/TF-TD/static/media/logo.25bf045c.svg"
+  }
+]);
